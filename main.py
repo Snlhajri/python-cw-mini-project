73e929a76aac5b8cb67bbb2bb549f73dba95ee3d
Snlhajri <Sumianh5@gmail.com>
2023-09-11 20:04:12 +0300

    cw5.py
    s

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ def rackets_cost (racket_brand) :
         return 200
     
 
- def padel_balls_cost(ball_boxes):
+def padel_balls_cost(ball_boxes):
     if  ball_boxes == 1 :
         return 2
     elif ball_boxes  ==2 :
         return 3.5
     if ball_boxes == 3 :
         return 5
+    
+def padel_game_cost():
+    court_tybe = input("enter court tybe: ") 
+    racket_brand = input("enter racket brand: ")
+    ball_boxes = int(input("enter number of ball boxes: "))
+    total= padel_court_cost(court_tybe)+rackets_cost(racket_brand) +padel_balls_cost(ball_boxes)
+    return total
+print(f"total cost {padel_game_cost()}")
 
 
 
